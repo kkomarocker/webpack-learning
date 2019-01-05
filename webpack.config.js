58e3494c0b4cc4e5d0fef14359bb9847543cf4eb
Jay.Lee <kkomarocker@gmail.com>
2019-01-05 01:44:31 -0500

    (feat): added css plugin to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,39 @@
 const webpack = require("webpack");
 const path = require("path");
+const MiniCssExtract = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./src/main.js",
+  entry: {
+    app: "./src/main.js"
+  },
+
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "bundle.js"
   },
+
   module: {
     rules: [
       {
         test: [/\.sass$/, /\.scss$/],
-        use: ["style-loader", "css-loader", "sass-loader"]
+        use: [
+          {
+            loader: MiniCssExtract.loader
+          },
+          "css-loader"
+        ]
       },
 
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
     ]
   },
+
   optimization: {
     minimize: false
   },
 
-  plugins: []
+  // need to use mini-css-extract-plugin to extract css when webpack v4 is in play.
+  plugins: [new MiniCssExtract("[name].css")]
 };
 
 if (process.env.NODE_ENV === "production") {
